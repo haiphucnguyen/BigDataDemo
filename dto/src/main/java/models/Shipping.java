13365cad2b759ea0998ef2f75ee3445dd4238fb6
AnhNghia Nguyen <nghianghesi@gmail.com>
2019-05-16 23:33:09 +0700

    order/shipping streamer

---
+++ dto/src/main/java/models/Shipping.java
@@ -0,0 +1,57 @@
+package models;
+
+import java.util.Date;
+import java.util.List;
+
+public class Shipping {
+	private String number;
+	private Date expectedDate;
+	private String user;
+	private String state;
+	private String city;
+	private String zipcode;
+	private List<OrderItem> items;
+	
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public Date getExpectedDate() {
+		return expectedDate;
+	}
+	public void setExpectedDate(Date expectedDate) {
+		this.expectedDate = expectedDate;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public List<OrderItem> getItems() {
+		return items;
+	}
+	public void setItems(List<OrderItem> items) {
+		this.items = items;
+	}
+}
