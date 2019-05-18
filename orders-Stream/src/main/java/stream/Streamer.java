ad4decd78e564e84a5afe5ae5903798d7ca92104
hainguyen <haiphucnguyen@gmail.com>
2019-05-18 11:05:16 -0500

    Update

--- orders-Stream/src/main/java/stream/Streamer.java
+++ orders-Stream/src/main/java/stream/Streamer.java
@@ -1,15 +1,10 @@
 package stream;
 
-import java.util.Arrays;
-import java.util.Properties;
-import java.util.concurrent.ExecutionException;
-
-import com.goyeau.kafka.streams.circe.CirceSerdes;
+import com.google.gson.Gson;
 import com.mekong.dto.Cart;
-import com.mekong.dto.Id;
 import com.mekong.dto.ShippingAddress;
 import com.mekong.dto.ShippingStatus;
-import io.circe.Encoder;
+import com.typesafe.config.Config;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.CreateTopicsResult;
@@ -18,87 +13,87 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.Serializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.gson.Gson;
-import com.typesafe.config.Config;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class Streamer {
-	
-	private String server;
-	private String orderTopic;
-	private String shippingTopic;
-	private String shipingStatusTopic;
-	private Producer<String, String> producer;
 
-	public Streamer(Config conf) {
-		server = conf.getString("kafka.server");
-		orderTopic = conf.getString("kafka.order");
-		shippingTopic = conf.getString("kafka.shipping");
-		shipingStatusTopic = conf.getString("kafka.shippingstatus");
-		this.ensureTopic(orderTopic);
-		this.ensureTopic(shippingTopic);
-		this.ensureTopic(shipingStatusTopic);		
-		this.createProducers();
-		logger.info("streamer {} {} {} {}", this.server, this.orderTopic, this.shippingTopic, this.shipingStatusTopic);
-	}	
-	
-	private static final Logger logger = LoggerFactory.getLogger(Streamer.class);
-	private static Gson gson = new Gson();
-	
-	private void ensureTopic(String topic) {
-		Properties props = new Properties();
-		props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, server);
-		AdminClient adminClient = AdminClient.create(props);
+    private String server;
+    private String orderTopic;
+    private String shippingTopic;
+    private String shipingStatusTopic;
+    private Producer<String, String> producer;
 
-		// by default, create topic with 1 partition, use Kafka tools to change this topic to scale.
-		NewTopic cTopic = new NewTopic(topic, 1, (short) 1);
-		CreateTopicsResult createTopicsResult = adminClient.createTopics(Arrays.asList(cTopic));
-		try {
-			createTopicsResult.all().get();
-		} catch (InterruptedException | ExecutionException e) {
-			logger.error("Create topic error {}", e.getMessage());
-		}
-	}
-	
-	
-	private void createProducers() {
-		Properties props = new Properties();
-		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-		this.producer = new KafkaProducer<>(props);
-	}
-	
-	private boolean sendData(String topic, String id, String data) {
-		ProducerRecord<String, String> record = new ProducerRecord<>(topic, id, data);
-		try {
-			logger.error("Sending message {} {}",topic, id);
+    public Streamer(Config conf) {
+        server = conf.getString("kafka.server");
+        orderTopic = conf.getString("kafka.order");
+        shippingTopic = conf.getString("kafka.shipping");
+        shipingStatusTopic = conf.getString("kafka.shippingstatus");
+        this.ensureTopic(orderTopic);
+        this.ensureTopic(shippingTopic);
+        this.ensureTopic(shipingStatusTopic);
+        this.createProducers();
+        logger.info("streamer {} {} {} {}", this.server, this.orderTopic, this.shippingTopic, this.shipingStatusTopic);
+    }
 
-			this.producer.send(record).get();
-			return true;
-		} catch (InterruptedException | ExecutionException e) {
-			// TODO Auto-generated catch block
-			logger.error("Sending message error {}", e.getMessage());
-			return false;
-		}
-	}
+    private static final Logger logger = LoggerFactory.getLogger(Streamer.class);
+    private static Gson gson = new Gson();
 
-	public boolean sendCart(Cart cart) {
-		logger.info("Sending order message");
-		return this.sendData(this.orderTopic, cart.cardId().toString(), gson.toJson(cart));
-	}
+    private void ensureTopic(String topic) {
+        Properties props = new Properties();
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, server);
+        AdminClient adminClient = AdminClient.create(props);
 
-	public boolean sendShipping(ShippingAddress shipping) {
-		logger.info("Sending shipping message");
-		return this.sendData(this.shippingTopic, shipping.cartId().toString(), gson.toJson(shipping));
-	}
+        // by default, create topic with 1 partition, use Kafka tools to change this topic to scale.
+        NewTopic cTopic = new NewTopic(topic, 1, (short) 1);
+        CreateTopicsResult createTopicsResult = adminClient.createTopics(Arrays.asList(cTopic));
+        try {
+            createTopicsResult.all().get();
+        } catch (InterruptedException | ExecutionException e) {
+            logger.error("Create topic error {}", e.getMessage());
+        }
+    }
 
-	public boolean sendShippingStatus(ShippingStatus status) {
-		logger.info("Sending shipping status message");
-		String data = gson.toJson(status);
-		return this.sendData(this.shipingStatusTopic, "" + status.orderId(), data);
-	}
+
+    private void createProducers() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        this.producer = new KafkaProducer<>(props);
+    }
+
+    private boolean sendData(String topic, String id, String data) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, id, data);
+        try {
+            logger.error("Sending message {} {}", topic, id);
+
+            this.producer.send(record).get();
+            return true;
+        } catch (InterruptedException | ExecutionException e) {
+            // TODO Auto-generated catch block
+            logger.error("Sending message error {}", e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean sendCart(Cart cart) {
+        logger.info("Sending order message");
+        return this.sendData(this.orderTopic, cart.cardId().toString(), gson.toJson(cart));
+    }
+
+    public boolean sendShipping(ShippingAddress shipping) {
+        logger.info("Sending shipping message");
+        return this.sendData(this.shippingTopic, shipping.cartId().toString(), gson.toJson(shipping));
+    }
+
+    public boolean sendShippingStatus(ShippingStatus status) {
+        logger.info("Sending shipping status message");
+        String data = gson.toJson(status);
+        return this.sendData(this.shipingStatusTopic, "" + status.orderId(), data);
+    }
 }
