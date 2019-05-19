5368e97cab3a23bda7c88a3ca8ef6ee2d3c1faf7
hainguyen <haiphucnguyen@gmail.com>
2019-05-18 23:41:58 -0500

    Update

--- spark-stream/src/main/scala/com/mekong/streaming/CartStreamingApp.scala
+++ spark-stream/src/main/scala/com/mekong/streaming/CartStreamingApp.scala
@@ -1,5 +1,6 @@
 package com.mekong.streaming
 
+import com.typesafe.config.ConfigFactory
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
@@ -7,14 +8,15 @@ import org.apache.spark.streaming.kafka010.KafkaUtils
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
-object StreamingApp {
+object CartStreamingApp {
 
   def main(args: Array[String]): Unit = {
-    val sparkConf = new SparkConf().setAppName("KafkaOrders").set("spark.ui.port", "8081")
+    val conf = ConfigFactory.load
+    val sparkConf = new SparkConf().setAppName("Cart Streaming Processing").set("spark.ui.port", "8081").setMaster("local[*]")
     val ssc = new StreamingContext(sparkConf, Seconds(1))
 
     val kafkaParams = Map[String, Object](
-      "bootstrap.servers" -> "kafka:9092,kafka:9093",
+      "bootstrap.servers" -> "localhost:9092,localhost:9093,kafka:9092,kafka:9093",
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[StringDeserializer],
       "group.id" -> "OrdersStream",
@@ -22,13 +24,13 @@ object StreamingApp {
       "enable.auto.commit" -> (false: java.lang.Boolean)
     )
 
-    val topics = Array("Orders")
+    val topics = Array("cart-topic", "shipping-topic")
     val stream = KafkaUtils.createDirectStream[String, String](
       ssc,
       PreferConsistent,
       Subscribe[String, String](topics, kafkaParams)
     )
-    stream.map(record => (record.key, record.value)).foreachRDD(r => println(r))
+    
     ssc.start()
     ssc.awaitTermination()
 
